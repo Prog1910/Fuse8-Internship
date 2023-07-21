07393cc75f59e5940cdc4e3b2620e0eb343546bd
Prog1910 <102432993+Prog1910@users.noreply.github.com>
2023-07-22 01:17:25 +0400

    feat: implement Animal inheritors
    Implemented/overridden methods and properties of inherited classes in Animals.cs

--- Homework2/Domain/Animals.cs
+++ Homework2/Domain/Animals.cs
@@ -5,48 +5,59 @@
 /// </summary>
 public abstract class Animal
 {
-	/// <summary>
-	/// true - если животное является другом человека
-	/// </summary>
-	public virtual bool IsHumanFriend => false;
+    /// <summary>
+    /// true - если животное является другом человека
+    /// </summary>
+    public virtual bool IsHumanFriend => false;
 
-	/// <summary>
-	/// true - если у животного большой вес
-	/// </summary>
-	public abstract bool HasBigWeight { get; }
+    /// <summary>
+    /// true - если у животного большой вес
+    /// </summary>
+    public abstract bool HasBigWeight { get; }
 
-	/// <summary>
-	/// Как говорит животное
-	/// </summary>
-	/// <returns>Возвращает звук, который говорит животное</returns>
-	public abstract string WhatDoesSay();
+    /// <summary>
+    /// Как говорит животное
+    /// </summary>
+    /// <returns>Возвращает звук, который говорит животное</returns>
+    public abstract string WhatDoesSay();
 }
-// TODO В наследниках реализовать метод WhatDoesSay и свойство HasBigWeight, а также переопределить IsHumanFriend там, где это нужно
 
 /// <summary>
 /// Собака
 /// </summary>
 public abstract class Dog : Animal
 {
+    public sealed override bool IsHumanFriend => true;
+
+    public sealed override string WhatDoesSay() => "гав";
 }
 
 /// <summary>
 /// Лиса
 /// </summary>
-public class Fox : Animal
+public sealed class Fox : Animal
 {
+    public override bool IsHumanFriend => false;
+
+    public override bool HasBigWeight => false;
+
+    public override string WhatDoesSay() => "ми-ми-ми";
 }
 
 /// <summary>
 /// Чихуахуа
 /// </summary>
-public class Chihuahua : Dog
+public sealed class Chihuahua : Dog
 {
+    public override bool HasBigWeight => false;
 }
 
 /// <summary>
 /// Хаски
 /// </summary>
-public class Husky : Dog
+public sealed class Husky : Dog
 {
+    public new string WhatDoesSay() => "ауф";
+
+    public override bool HasBigWeight => true;
 }
