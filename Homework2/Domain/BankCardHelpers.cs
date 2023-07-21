1531a235f97e3d852da437bbe441a976ff5159f2
Prog1910 <102432993+Prog1910@users.noreply.github.com>
2023-07-21 23:23:05 +0400

    feat: implement GetUnmaskedCardNumber
    Implemented BankCardHelpers.GetUnmaskedCardNumber getting private property from BankCard using reflection

--- Homework2/Domain/BankCardHelpers.cs
+++ Homework2/Domain/BankCardHelpers.cs
@@ -4,14 +4,11 @@ namespace Fuse8_ByteMinds.SummerSchool.Domain;
 
 public static class BankCardHelpers
 {
-	/// <summary>
-	/// Получает номер карты без маски
-	/// </summary>
-	/// <param name="card">Банковская карта</param>
-	/// <returns>Номер карты без маски</returns>
-	public static string GetUnmaskedCardNumber(BankCard card)
-	{
-		// TODO С помощью рефлексии получить номер карты без маски
-		return card.MaskedCardNumber;
-	}
+    /// <summary>
+    /// Получает номер карты без маски
+    /// </summary>
+    /// <param name="card">Банковская карта</param>
+    /// <returns>Номер карты без маски</returns>
+    public static string GetUnmaskedCardNumber(BankCard card)
+        => typeof(BankCard).GetField("_number", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(card)?.ToString() ?? string.Empty;
 }
