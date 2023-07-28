06e14191204d3ea9f59a0dd18f58f6f33fa4dbd6
Prog <shirali.salimov.2002@gmail.com>
2023-07-28 22:13:44 +0400

    refactor: add taking default currencies
    Added taking default currencies from the application configuration and changed routes

--- Homework3/CurrencyApi/PublicApi/Controllers/CurrencyController.cs
+++ Homework3/CurrencyApi/PublicApi/Controllers/CurrencyController.cs
@@ -8,7 +8,10 @@ using System.Text.Json;
 
 namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Controllers;
 
-[Route("currencyapi")]
+/// <summary>
+/// Methods for handling exchange rate conversion
+/// </summary>
+[Route("currencyapi/")]
 public class CurrencyController : Controller
 {
 	private readonly CurrencyApiSettings _currencyServiceSettings;
@@ -20,21 +23,22 @@ public class CurrencyController : Controller
 		_httpClient = httpClient;
 	}
 
+	#region GET /currency
 	/// <summary>
-	/// Latest Сurrency Exchange Data (default currency RUB, default base currency USD).
+	/// Latest Сurrency Exchange Data (default currency RUB, default base currency USD)
 	/// </summary>
 	/// <response code="200">
-	/// Returns if it was possible to get the default currency data.	
+	/// Returns if it was possible to get the default currency data
 	/// </response>
 	/// <response code="500">
-	/// Returns if the default currency data could not be retrieved.
+	/// Returns if the default currency data could not be retrieved
 	/// </response>
-	[HttpGet]
-	[Route("currency")]
-	public async Task<CurrencyDataDto> GetLatestExchangeRates()
+	[HttpGet("currency")]
+	public async Task<CurrencyDataDto> GetCurrencyExchangeRate()
 	{
 		_httpClient.DefaultRequestHeaders.Add("apikey", _currencyServiceSettings.ApiKey);
-		var responseMessage = await _httpClient.GetAsync("https://api.currencyapi.com/v3/latest?currencies=RUB&base_currency=USD");
+		var requestUri = $"https://api.currencyapi.com/v3/latest?currencies={_currencyServiceSettings.DefaultCurrency}&base_currency={_currencyServiceSettings.BaseCurrency}";
+		var responseMessage = await _httpClient.GetAsync(requestUri);
 		if (responseMessage.IsSuccessStatusCode)
 		{
 			var responseContent = await responseMessage.Content.ReadAsStringAsync();
@@ -47,19 +51,67 @@ public class CurrencyController : Controller
 		}
 		throw new HttpRequestException("Failed to get default currency data", null, HttpStatusCode.InternalServerError);
 	}
+	#endregion
 
+	#region GET /currency/{currencyCode}
 	/// <summary>
-	/// The status endpoint returns information about your current quota.
+	/// Latest Сurrency Exchange Data (default base currency USD)
 	/// </summary>
+	/// <param name="currencyCode">Сurrency the current rate of which will be returned</param>
 	/// <response code="200">
-	/// Returns if it was possible to get the information about your current quota.	
+	/// Returns if data was received successfully currency passed as a parameter
 	/// </response>
 	/// <response code="500">
-	/// Returns if the information about your current quota could not be retrieved.
+	/// Returns if the data could not be retrieved currency passed as a parameter
 	/// </response>
-	[HttpGet]
-	[Route("settings")]
-	public async Task<CurrentStatusDto> ChechApiStatus()
+	[HttpGet("/currency/{currencyCode}")]
+	public async Task<CurrencyDataDto> GetCurrencyExchangeRateByCode(string currencyCode)
+	{
+		_httpClient.DefaultRequestHeaders.Add("apikey", _currencyServiceSettings.ApiKey);
+		var validCurrencyCode = currencyCode.ToUpper();
+		var requestUri = $"https://api.currencyapi.com/v3/latest?currencies={validCurrencyCode}&base_currency={_currencyServiceSettings.BaseCurrency}";
+		var responseMessage = await _httpClient.GetAsync(requestUri);
+		if (responseMessage.IsSuccessStatusCode)
+		{
+			var responseContent = await responseMessage.Content.ReadAsStringAsync();
+			var currencyResponse = JsonSerializer.Deserialize<CurrencyResponseDto>(responseContent);
+			if (currencyResponse is not null)
+			{
+				var currencyData = currencyResponse.CurrenciesData[validCurrencyCode];
+				return new(currencyData.Code, Math.Round(currencyData.Value, _currencyServiceSettings.CurrencyRoundCount));
+			}
+		}
+		throw new HttpRequestException("Failed to get default currency data", null, HttpStatusCode.InternalServerError);
+	}
+	#endregion
+
+	#region GET /currency/{currencyCode}/{date}
+	/*
+	 * GET /currency/{currencyCode}/{date}
+	{date} должна иметь формат yyyy-MM-dd
+	Должен вызывать метод внешнего API https://api.currencyapi.com/v3/historical?currencies={currencyCode}&date={date}&base_currency=USD согласно документации
+	Должен возвращать курс валюты, переданной в качестве параметра, на определенную дата
+	Базовую валюту USD необходимо брать из конфигурации приложения.
+	Курс должен округляться до двух знаков после зяпятой. Количество знаков следует брать из конфигурации приложения.
+	Должен возвращать JSON вида (дата должна иметь формат yyyy-MM-dd)
+	*/
+	//[HttpGet("/currency/{currencyCode}")]
+
+
+	#endregion
+
+	#region GET /settings
+	/// <summary>
+	/// The status endpoint returns information about your current quota
+	/// </summary>
+	/// <response code="200">
+	/// Returns if it was possible to get the information about your current quota
+	/// </response>
+	/// <response code="500">
+	/// Returns if the information about your current quota could not be retrieved
+	/// </response>
+	[HttpGet("settings")]
+	public async Task<CurrentStatusDto> GetApiSettings()
 	{
 		_httpClient.DefaultRequestHeaders.Add("apikey", _currencyServiceSettings.ApiKey);
 		var requestUri = "https://api.currencyapi.com/v3/status";
@@ -80,4 +132,5 @@ public class CurrencyController : Controller
 		}
 		throw new HttpRequestException("Failed to get current settings", null, HttpStatusCode.InternalServerError);
 	}
+	#endregion
 }
