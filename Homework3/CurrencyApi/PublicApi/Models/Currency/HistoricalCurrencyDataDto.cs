5fe4934765c0e743c00d247f0cb7c73bb3fbb657
Prog <shirali.salimov.2002@gmail.com>
2023-07-29 12:59:28 +0400

    feat: add HistoricalCurrencyDataDto
    Add HistoricalCurrencyDataDto for GET /currency/{currencyCode}/{date} method

---
+++ Homework3/CurrencyApi/PublicApi/Models/Currency/HistoricalCurrencyDataDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Models.Currency;
+
+/// <summary>
+/// Historical currency information
+/// </summary>
+/// <param name="Date">Date of exchange rate relevance</param>
+/// <param name="Code">Currency code</param>
+/// <param name="Value">Currency rate</param>
+public record HistoricalCurrencyDataDto(
+	[property: JsonPropertyName("date")] string Date,
+	string Code,
+	decimal Value
+	) : CurrencyDataDto(Code, Value);
