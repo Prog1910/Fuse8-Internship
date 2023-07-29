3ce4dde0c8875508dc1da2265a9116b39ffedc22
Prog <shirali.salimov.2002@gmail.com>
2023-07-29 14:33:53 +0400

    refactor: move dto

--- Homework3/CurrencyApi/PublicApi/Models/Status/CurrentStatusDto.cs
+++ Homework3/CurrencyApi/PublicApi/Models/Status/CurrentStatusDto.cs
@@ -1,9 +1,9 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Models.Currency;
+namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Models.Status;
 
 /// <summary>
-/// Current application settings
+/// Current API settings
 /// </summary>
 /// <param name="DefaultCurrency">Current default exchange rate from configuration</param>
 /// <param name="BaseCurrency">Base currency against which the exchange rate is calculated</param>
@@ -11,9 +11,9 @@ namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Models.Currency;
 /// <param name="RequestCount">Number of used requests received from external API</param>
 /// <param name="CurrencyRoundCount">The number of decimal places to which the value of the exchange rate should be rounded</param>
 public record CurrentStatusDto(
-	[property: JsonPropertyName("default_currency")] string DefaultCurrency,
-	[property: JsonPropertyName("base_currency")] string BaseCurrency,
-	[property: JsonPropertyName("request_limit")] int RequestLimit,
-	[property: JsonPropertyName("request_count")] int RequestCount,
-	[property: JsonPropertyName("currency_round_count")] int CurrencyRoundCount
-	);
+    [property: JsonPropertyName("default_currency")] string DefaultCurrency,
+    [property: JsonPropertyName("base_currency")] string BaseCurrency,
+    [property: JsonPropertyName("request_limit")] int RequestLimit,
+    [property: JsonPropertyName("request_count")] int RequestCount,
+    [property: JsonPropertyName("currency_round_count")] int CurrencyRoundCount
+    );
