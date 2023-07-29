be173fe94bcc1d06032d5de298497e283dd98407
Prog <shirali.salimov.2002@gmail.com>
2023-07-29 12:58:08 +0400

    chore: format code

--- Homework3/CurrencyApi/PublicApi/Models/Currency/CurrencyDataDto.cs
+++ Homework3/CurrencyApi/PublicApi/Models/Currency/CurrencyDataDto.cs
@@ -8,6 +8,6 @@ namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Models.Currency;
 /// <param name="Code">Currency code</param>
 /// <param name="Value">Currency rate</param>
 public record CurrencyDataDto(
-    [property: JsonPropertyName("code")] string Code,
-    [property: JsonPropertyName("value")] decimal Value
-    );
+	[property: JsonPropertyName("code")] string Code,
+	[property: JsonPropertyName("value")] decimal Value
+	);
