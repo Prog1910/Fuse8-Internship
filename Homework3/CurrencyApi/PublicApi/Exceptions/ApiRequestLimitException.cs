f028c0dd226e5da3728463b8b4e4476ea59ffb2b
Prog <shirali.salimov.2002@gmail.com>
2023-07-29 16:52:53 +0400

    feat: implement ApiRequestLimitException

---
+++ Homework3/CurrencyApi/PublicApi/Exceptions/ApiRequestLimitException.cs
@@ -0,0 +1,8 @@
+﻿namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Exceptions;
+
+public class ApiRequestLimitException : Exception
+{
+	public ApiRequestLimitException() : base("You have reached your request limit") { }
+
+	public ApiRequestLimitException(string? message) : base(message) { }
+}
