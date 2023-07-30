b39a71462be3dfa06c413c72bd3168e73bb42bfe
Prog <shirali.salimov.2002@gmail.com>
2023-07-30 12:17:18 +0400

    feat: implement HttpResponseMessage extensions

---
+++ Homework3/CurrencyApi/PublicApi/Extensions/HttpResponseExtenstions.cs
@@ -0,0 +1,30 @@
+﻿using Fuse8_ByteMinds.SummerSchool.PublicApi.Exceptions;
+using Fuse8_ByteMinds.SummerSchool.PublicApi.Models;
+using System.Net;
+using System.Text.Json;
+
+namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Extensions;
+
+public static class HttpResponseMessageExtenstions
+{
+	public async static Task<TDto> EnsureValidAndDeserialize<TDto>(this HttpResponseMessage responseMessage) where TDto : IInfo
+	{
+		EnsureValid(responseMessage);
+		var response = await responseMessage.Deserialize<TDto>();
+		return response is not null ? response : throw new CurrencyNotFoundException();
+	}
+
+	public static void EnsureValid(this HttpResponseMessage responseMessage)
+	{
+		if (responseMessage.IsSuccessStatusCode == false)
+			throw responseMessage.StatusCode switch
+			{
+				HttpStatusCode.TooManyRequests => new ApiRequestLimitException(),
+				HttpStatusCode.UnprocessableEntity => new CurrencyNotFoundException(),
+				_ => new HttpRequestException(message: "Failed to get default currency data", inner: null, statusCode: HttpStatusCode.InternalServerError),
+			};
+	}
+
+	public static async Task<TDto?> Deserialize<TDto>(this HttpResponseMessage responseMessage)
+		=> JsonSerializer.Deserialize<TDto>(await responseMessage.Content.ReadAsStringAsync());
+}
