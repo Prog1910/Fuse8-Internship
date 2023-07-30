1a4ee517614913c38b028e558cdc3998818b292a
Prog <shirali.salimov.2002@gmail.com>
2023-07-30 18:06:00 +0400

    refactor: change api status dto

--- Homework3/CurrencyApi/PublicApi/Models/Status/QuotaInfoDto.cs
+++ Homework3/CurrencyApi/PublicApi/Models/Status/QuotaInfoDto.cs
@@ -7,7 +7,7 @@ namespace Fuse8_ByteMinds.SummerSchool.PublicApi.Models.Status;
 /// </summary>
 /// <param name="AccountId">Your unique account identifier</param>
 /// <param name="Quotas">Contains information about your request quota</param>
-public record ApiStatusDto(
-	[property: JsonPropertyName("account_id")] Guid AccountId,
+public record QuotaInfoDto(
+	[property: JsonPropertyName("account_id")] long AccountId,
 	[property: JsonPropertyName("quotas")] QuotasDto Quotas
-	);
+	) : IInfo;
